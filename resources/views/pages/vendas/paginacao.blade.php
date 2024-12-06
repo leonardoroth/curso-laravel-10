a3ee947dc91d3ed3bb71b32f8cd28815d6f65670
Leonardo Soares <127786348+leonardoroth@users.noreply.github.com>
2024-12-06 10:21:27 -0300

    Update paginacao.blade.php

--- resources/views/pages/vendas/paginacao.blade.php
+++ resources/views/pages/vendas/paginacao.blade.php
@@ -2,7 +2,7 @@
 
 @Section ('content')
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-      <h1 class="h2">Clientes</h1>
+      <h1 class="h2">Produtos</h1>
   </div>
 
   <div>
@@ -15,41 +15,24 @@
       </form>
 
   <div class="table-responsive mt-4">
-    @if ($findVenda->isEmpty())
+    @if ($findVendas->isEmpty())
       <p> Não existem dados </p>   
     @else
       <table class="table table-striped table-sm">
         <thead>
           <tr>
-            <th>Numeração</th>
-            <th>E-mail</th>
-            <th>Endereço</th>
-            <th>Logradouro</th>
-            <th>CEP</th>
-            <th>Bairro</th>
+            <th>Nome</th>
+            <th>Produto</th>
+            <th>Cliente</th>
             <th>Ações</th>
           </tr>
         </thead>
         <tbody>
-          @foreach ($findCliente as $cliente)
+          @foreach ($findVendas as $venda)
               <tr>
-                  <td> {{ $cliente->nome }} </td>
-                  <td> {{ $cliente->email }} </td>
-                  <td> {{ $cliente->endereço }} </td>
-                  <td> {{ $cliente->logradouro }} </td>
-                  <td> {{ $cliente->cep }} </td>
-                  <td> {{ $cliente->bairro }} </td>
-
-                  <td>
-                      <a href=" {{ route('atualizar.Clientes', $cliente->id) }}" class="btn btn-light btn-sm"> 
-                        Editar
-                      </a>
-                      <meta name="csrf-token" content=" {{ csrf_token() }}"/>
-                      <a onclick="deleteRegistroPaginacao( '{{ route('clientes.delete') }} ', {{ $cliente->id }} )"
-                        class="btn btn-danger btn-sm">
-                        Excluir
-                      </a>
-                  </td>
+                  <td> {{ $venda->numero_da_venda }} </td>
+                  <td> {{ $venda->produto->nome }} </td>
+                  <td> {{ $venda->cliente->nome }} </td>
               </tr>
           @endforeach
 
