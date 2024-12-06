3da8fca7be48b1230f70545940fc3d5a1084d777
Leonardo Soares <127786348+leonardoroth@users.noreply.github.com>
2024-12-06 13:22:58 -0300

    Update create.blade.php

--- resources/views/pages/vendas/create.blade.php
+++ resources/views/pages/vendas/create.blade.php
@@ -1,26 +1,28 @@
 @extends('index')
 
 @section('content')
-    <form class="form" method="POST" action="{{ route('cadastrar.produto') }}">
+    <form class="form" method="POST" action="{{ route('cadastrar.venda') }}">
         @csrf
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Criar novo Produto</h1>
+            <h1 class="h2">Cadastrar nova venda</h1>
         </div>
         <div class="mb-3">
-            <label class="form-label">Nome</label>
-            <input type="text" value="{{ old('nome') }}" class="form-control @error('nome') is-invalid @enderror"
-                name="nome">
+            <label class="form-label">Numeração</label>
+            <input type="text" disabled value="{{ old('numero_da_venda') }}" class="form-control @error('numero_da_venda') is-invalid @enderror"
+                name="numero_da_venda">
             @if ($errors->has('nome'))
                 <div class="invalid-feedback"> {{ $errors->first('nome') }}</div>
             @endif
         </div>
+
         <div class="mb-3">
-            <label class="form-label">Valor</label>
-            <input id='mascara_valor' value="{{ old('valor') }}" class="form-control @error('valor') is-invalid @enderror"
-                name="valor">
-            @if ($errors->has('valor'))
-                <div class="invalid-feedback"> {{ $errors->first('valor') }}</div>
-            @endif
+            <label class="form-label"> Produto </label>
+                <select class="form-select" name="">
+                    <option selected>Clique para abrir o menu de seleção</option>
+                        @foreach ($findProduto as $produto)
+                            <option value=" {{ $produto->id }}">One</option>
+                        @endforeach
+                </select>
         </div>
 
         <button type="submit" class="btn btn-success">GRAVAR</button>
